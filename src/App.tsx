5312f0f5a9efd8ca962dcb6a1b2b4d4119c17659
sam <samt@samt.st>
2024-02-23 17:19:42 -0300

    start playing with making invoices

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import type * as WebLNTypes from "@webbtc/webln-types";
 
 function App() {
+
+  const makeInvoice = async () => {
+    if (window.webln) {
+      (async () => {
+        // @ts-ignore
+        await window.webln.enable();
+        
+        // @ts-ignore
+        const invoice = await window.webln.makeInvoice({
+          amount: 2121,
+        });
+
+        const invoiceString = invoice.paymentRequest
+
+        console.log('invoiceString', invoiceString)
+
+
+      })();
+    } else {
+      console.warn("WebLN not enabled");
+    }
+  }
   return (
     <div>
      [todo: build app]
+     <p>
+      <button onClick={makeInvoice}>make invoice</button>
+     </p>
     </div>
   );
 }
